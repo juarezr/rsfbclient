9aa2e3c5ca7ec834f0c24c1a1aa0a85a43edfa6a
Juarez Rudsatz <juarez.rudsatz@ceabs.net>
2020-08-13 20:57:41 -0300

    ci: change build.rs with cargo:rustc-link-lib=fbclient

--- rsfbclient-native/build.rs
+++ rsfbclient-native/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     #[cfg(feature = "linking")]
-    println!("cargo:rustc-link-lib=dylib=fbclient");
+    // println!("cargo:rustc-link-lib=dylib=fbclient");
+    println!("cargo:rustc-link-lib=fbclient");
 }
